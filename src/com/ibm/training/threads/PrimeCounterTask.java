e99f42b6b6201f9b635bcf04f74da9ce1062fb5d
Pradeep LN <pradeep.ln78@gmail.com>
2024-05-24 13:03:09 +0530

    eod 15

---
+++ src/com/ibm/training/threads/PrimeCounterTask.java
@@ -0,0 +1,44 @@
+package com.ibm.training.threads;
+
+public class PrimeCounterTask implements Runnable{
+
+	long count;
+	
+	long begin,end;
+	
+	public PrimeCounterTask(long begin, long end) {
+		super();
+		this.begin = begin;
+		this.end = end;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("STarting prime counting from "+begin+" to "+end+" in "+Thread.currentThread().getName());
+		
+		for(long i = begin; i <= end; i++) {
+			if(isPrime(i)) {
+				count++;
+				System.out.println("Found a prime "+i+" in thread "+Thread.currentThread().getName());
+			}
+		}
+		System.out.println("Finished prime counting from "+begin+" to "+end+" in "+Thread.currentThread().getName());
+	}
+	
+	public static boolean isPrime(long num) {
+		if (num <= 2) {
+			return true;
+		}
+		for (long i = 2; i < num; i++) {
+			if ((num % i) == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	public long getCount() {
+		return count;
+	}
+}
